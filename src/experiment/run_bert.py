bec83cb115d364c29fb27cffe0605901dcf6538f
Dhanya Sridhar <dsridhar@ucsc.edu>
2022-02-16 09:04:21 -0500

    finalizing bert

--- src/experiment/run_bert.py
+++ src/experiment/run_bert.py
@@ -51,13 +51,11 @@ class BERTClass(torch.nn.Module):
 	def __init__(self):
 		super(BERTClass, self).__init__()
 		self.l1 = transformers.BertModel.from_pretrained('bert-base-uncased')
-		# self.l2 = torch.nn.Dropout(0.3)
-		self.l3 = torch.nn.Linear(768, 1)
+		self.l2 = torch.nn.Linear(768, 1)
 	
 	def forward(self, ids, mask):
 		output_1 = self.l1(ids, mask)[0]
-		# output_2 = self.l2(output_1.mean(1))
-		output = self.l3(output_1.mean(1))
+		output = self.l2(output_1.mean(1))
 		return output
 
 
